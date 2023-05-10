4a0f7e7cd40145a53b5a7f1f9d658274882a04ef
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-05-10 11:55:42 +0700

    fix: database

--- connection.js
+++ connection.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql');
 
+require('dotenv').config();
+
+const {DB_HOST,DB_PASSWORD,DB_USER,DB_NAME}=process.env;
+
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'pokedex'
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
   });
   
   connection.connect((error) => {
