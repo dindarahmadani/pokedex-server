032a39cee0ab9e09e56a7decd4cbc41c78b1b17a
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-05-23 17:29:08 +0700

    fix: add method Get Users

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../connection');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  var sql = "SELECT * FROM users";
+  connection.query(sql, function(err, results) {
+    if (err) throw err;
+    res.json(results);
+  });
 });
 
 module.exports = router;
