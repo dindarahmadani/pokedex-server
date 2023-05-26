e252dab36f0a1d79872606a42bd8770b3b37d79a
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-05-26 10:07:32 +0700

    fix: add route my pokemons

--- routes/pokemons.js
+++ routes/pokemons.js
@@ -1,16 +1,20 @@
 var express = require('express');
 var router = express.Router();
 var pokemonController = require('../controllers/pokemons');
+var myPokemonController = require('../controllers/mypokemons');
 
 router.get('/pokemons', pokemonController.getListPokemons);
+router.get('/pokemons/collection', myPokemonController.getListMyPokemons);
 
 router.get('/pokemons/:id', pokemonController.getPokemonById);
 
 router.post('/pokemons', pokemonController.addPokemon);
+router.post('/pokemons/collection', myPokemonController.addToMyPokemon);
 
 router.put('/pokemons/:id', pokemonController.updatePokemon);
 
 router.delete('/pokemons/:id', pokemonController.deletePokemon);
+router.delete('/pokemons/collection/:id', myPokemonController.deleteMyPokemon);
 
 
 module.exports = router;
