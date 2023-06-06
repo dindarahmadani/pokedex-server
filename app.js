742fcaa95c03fe0b025354cceff588f805e060ac
[Dinda Rahmadani] <[dindarahmadani271@gmail.com]>
2023-06-06 07:04:16 +0700

    feat: install cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -27,6 +28,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const corsOptions = {
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+};
+
+app.use(cors(corsOptions));
 app.use('/', indexRouter);
 app.use(usersRouter);
 app.use(pokemonsRouter);
